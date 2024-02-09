82f7f632e4af1e3fd381dde5277800b8def21c46
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-02-09 02:57:55 -0400

    Rocket Launcher Bugfix
    Rocket Launcher won't fire when exiting menu.

--- Assets/Scripts/RocketLauncher.cs
+++ Assets/Scripts/RocketLauncher.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,11 +9,12 @@ public class RocketLauncher : MonoBehaviour
     [Header("Object References")]
     [SerializeField] private GameObject rocketPrefab;
     [SerializeField] private Transform launchPos;
+    [ReadOnly(true)] private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
@@ -30,7 +32,7 @@ public class RocketLauncher : MonoBehaviour
     //function that instantiates a rocket and launches it
     public void Fire(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && !gameManager.isGamePaused())
         {
             Instantiate(rocketPrefab, launchPos.position, launchPos.rotation);
         }
